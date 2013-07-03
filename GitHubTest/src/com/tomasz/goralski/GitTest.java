7397699436c7f4d4ddd60a571b8ca6a9569b3f06
Tomasz Goralski <tomasz_goralski@wp.pl>
2013-07-03 21:56:07 +0200

    next commit

--- GitHubTest/src/com/tomasz/goralski/GitTest.java
+++ GitHubTest/src/com/tomasz/goralski/GitTest.java
@@ -4,10 +4,16 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Git !");
 		GitTest.PrintHello();
+
+		Test2 test2 = new Test2();
+		test2.setAge("12");
+		test2.setName("tomek");
+
+		System.out.println(test2.getName());
 	}
 
 	public static void PrintHello() {
 		System.out.println("HelloWorld");
 	}
-	
+
 }
