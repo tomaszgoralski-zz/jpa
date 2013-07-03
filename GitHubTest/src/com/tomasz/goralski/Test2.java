a9281c83657ded7564867b951d1b97e84faab6a9
Tomasz Goralski <tomasz_goralski@wp.pl>
2013-07-03 21:22:50 +0200

    Added Test2

---
+++ GitHubTest/src/com/tomasz/goralski/Test2.java
@@ -0,0 +1,32 @@
+package com.tomasz.goralski;
+
+public class Test2 {
+
+	private String name;
+	private String age;
+	private Long id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
