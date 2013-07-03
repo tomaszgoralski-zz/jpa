80e07e6fee40536d6a47e1b28a87262207756cbb
Tomasz Goralski <tomasz_goralski@wp.pl>
2013-07-03 21:24:13 +0200

    Added new methods in GitTest

--- GitHubTest/src/com/tomasz/goralski/GitTest.java
+++ GitHubTest/src/com/tomasz/goralski/GitTest.java
@@ -3,5 +3,11 @@ package com.tomasz.goralski;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Git !");
+		GitTest.PrintHello();
 	}
+
+	public static void PrintHello() {
+		System.out.println("HelloWorld");
+	}
+	
 }
